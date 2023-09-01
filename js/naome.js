ac68cec45e09664e51873f15d1ccbf8fe242142d
Anaswageh <134636135+Anaswageh@users.noreply.github.com>
2023-09-01 19:11:01 +0300

    init

---
+++ js/naome.js
@@ -0,0 +1,13 @@
+/*$(document).ready(function(){
+    
+    
+    $(".left-button").on( "click", function(){
+      
+        $(".body-left").toggleClass("slide-right ");
+        $(".body-right").toggleClass("opacity-right ");
+      
+      
+    });
+     
+  
+    }); */
